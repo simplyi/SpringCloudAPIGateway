c775736aaa39e1ddff12b65bf4195cdea1666f6e
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2020-12-18 11:19:58 -0500

    Created a new project and have registered it with Eureka client

---
+++ src/test/java/com/appsdeveloperblog/photoapp/api/gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.photoapp.api.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
