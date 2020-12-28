15d3a1c092bcadd965865bb019cc00edca784311
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2020-12-28 17:36:43 -0500

    Handled Exception(s) for the Jwts Parser

--- src/main/java/com/appsdeveloperblog/photoapp/api/gateway/AuthorizationHeaderFilter.java
+++ src/main/java/com/appsdeveloperblog/photoapp/api/gateway/AuthorizationHeaderFilter.java
@@ -17,14 +17,14 @@ import reactor.core.publisher.Mono;
 
 @Component
 public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<AuthorizationHeaderFilter.Config> {
-	
+
 	@Autowired
 	Environment env;
-	
+
 	public AuthorizationHeaderFilter() {
 		super(Config.class);
 	}
-	
+
 	public static class Config {
 		// Put configuration properties here
 	}
@@ -32,45 +32,48 @@ public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<Auth
 	@Override
 	public GatewayFilter apply(Config config) {
 		return (exchange, chain) -> {
-			
+
 			ServerHttpRequest request = exchange.getRequest();
-			
-			if(!request.getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+
+			if (!request.getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
 				return onError(exchange, "No authorization header", HttpStatus.UNAUTHORIZED);
 			}
-			
+
 			String authorizationHeader = request.getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
 			String jwt = authorizationHeader.replace("Bearer", "");
-			
-			if(!isJwtValid(jwt)) {
+
+			if (!isJwtValid(jwt)) {
 				return onError(exchange, "JWT token is not valid", HttpStatus.UNAUTHORIZED);
 			}
-			
+
 			return chain.filter(exchange);
 		};
 	}
-	
-    private Mono<Void> onError(ServerWebExchange exchange, String err, HttpStatus httpStatus) {
-        ServerHttpResponse response = exchange.getResponse();
-        response.setStatusCode(httpStatus);
 
-        return response.setComplete();
-    }
-    
-    private boolean isJwtValid(String jwt) {
-    	boolean returnValue = true;
-    	
-    	String subject = Jwts.parser()
-    	.setSigningKey(env.getProperty("token.secret"))
-    	.parseClaimsJws(jwt)
-    	.getBody()
-    	.getSubject();
-    	
-    	if(subject == null || subject.isEmpty()) {
-    		returnValue = false;
-    	}
-    	
-    	return returnValue;
-    }
+	private Mono<Void> onError(ServerWebExchange exchange, String err, HttpStatus httpStatus) {
+		ServerHttpResponse response = exchange.getResponse();
+		response.setStatusCode(httpStatus);
+
+		return response.setComplete();
+	}
+
+	private boolean isJwtValid(String jwt) {
+		boolean returnValue = true;
+
+		String subject = null;
+
+		try {
+			subject = Jwts.parser().setSigningKey(env.getProperty("token.secret")).parseClaimsJws(jwt).getBody()
+					.getSubject();
+		} catch (Exception ex) {
+			returnValue = false;
+		}
+
+		if (subject == null || subject.isEmpty()) {
+			returnValue = false;
+		}
+
+		return returnValue;
+	}
 
 }
