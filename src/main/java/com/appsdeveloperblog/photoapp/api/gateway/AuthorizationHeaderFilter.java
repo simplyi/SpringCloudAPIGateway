18ebda4f55045dbf62f56a4ff626ac195b1c85a7
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2020-12-28 13:44:49 -0500

    Validating JWT

--- src/main/java/com/appsdeveloperblog/photoapp/api/gateway/AuthorizationHeaderFilter.java
+++ src/main/java/com/appsdeveloperblog/photoapp/api/gateway/AuthorizationHeaderFilter.java
@@ -1,7 +1,9 @@
 package com.appsdeveloperblog.photoapp.api.gateway;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
@@ -10,11 +12,14 @@ import org.springframework.web.server.ServerWebExchange;
 
 import com.google.common.net.HttpHeaders;
 
+import io.jsonwebtoken.Jwts;
 import reactor.core.publisher.Mono;
 
 @Component
 public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<AuthorizationHeaderFilter.Config> {
 	
+	@Autowired
+	Environment env;
 	
 	public static class Config {
 		// Put configuration properties here
@@ -27,11 +32,15 @@ public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<Auth
 			ServerHttpRequest request = exchange.getRequest();
 			
 			if(!request.getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
-				return onError(exchange, "No authorization header",HttpStatus.UNAUTHORIZED);
+				return onError(exchange, "No authorization header", HttpStatus.UNAUTHORIZED);
 			}
 			
 			String authorizationHeader = request.getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
 			String jwt = authorizationHeader.replace("Bearer", "");
+			
+			if(!isJwtValid(jwt)) {
+				return onError(exchange, "JWT token is not valid", HttpStatus.UNAUTHORIZED);
+			}
 			
 			return chain.filter(exchange);
 		};
@@ -42,6 +51,22 @@ public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<Auth
         response.setStatusCode(httpStatus);
 
         return response.setComplete();
+    }
+    
+    private boolean isJwtValid(String jwt) {
+    	boolean returnValue = true;
+    	
+    	String subject = Jwts.parser()
+    	.setSigningKey(env.getProperty("token.secret"))
+    	.parseClaimsJws(jwt)
+    	.getBody()
+    	.getSubject();
+    	
+    	if(subject == null || subject.isEmpty()) {
+    		returnValue = false;
+    	}
+    	
+    	return returnValue;
     }
 
 }
