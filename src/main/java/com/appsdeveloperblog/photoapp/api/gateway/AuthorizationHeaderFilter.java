1850ac1a2a89bd819d3dd8b6170095633a9ab451
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2020-12-28 11:59:02 -0500

    Reading the Authorization Header and the JWT in the AuthorizationHeaderFilter class.

--- src/main/java/com/appsdeveloperblog/photoapp/api/gateway/AuthorizationHeaderFilter.java
+++ src/main/java/com/appsdeveloperblog/photoapp/api/gateway/AuthorizationHeaderFilter.java
@@ -2,7 +2,15 @@ package com.appsdeveloperblog.photoapp.api.gateway;
 
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import com.google.common.net.HttpHeaders;
+
+import reactor.core.publisher.Mono;
 
 @Component
 public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<AuthorizationHeaderFilter.Config> {
@@ -14,8 +22,26 @@ public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<Auth
 
 	@Override
 	public GatewayFilter apply(Config config) {
-		// TODO Auto-generated method stub
-		return null;
+		return (exchange, chain) -> {
+			
+			ServerHttpRequest request = exchange.getRequest();
+			
+			if(!request.getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+				return onError(exchange, "No authorization header",HttpStatus.UNAUTHORIZED);
+			}
+			
+			String authorizationHeader = request.getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
+			String jwt = authorizationHeader.replace("Bearer", "");
+			
+			return chain.filter(exchange);
+		};
 	}
+	
+    private Mono<Void> onError(ServerWebExchange exchange, String err, HttpStatus httpStatus) {
+        ServerHttpResponse response = exchange.getResponse();
+        response.setStatusCode(httpStatus);
+
+        return response.setComplete();
+    }
 
 }
