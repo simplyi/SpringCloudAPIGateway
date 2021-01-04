f63f7cf5525cb72eed37dcc7f8a8651c13570b84
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2021-01-04 09:15:44 -0500

    Global Filter to log HttpHeaders

--- src/main/java/com/appsdeveloperblog/photoapp/api/gateway/MyPreFilter.java
+++ src/main/java/com/appsdeveloperblog/photoapp/api/gateway/MyPreFilter.java
@@ -1,17 +1,42 @@
 package com.appsdeveloperblog.photoapp.api.gateway;
 
+import java.util.Set;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 
 import reactor.core.publisher.Mono;
 
+@Component
 public class MyPreFilter implements GlobalFilter {
+	
+	final Logger logger = LoggerFactory.getLogger(MyPreFilter.class);
 
 	@Override
 	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		logger.info("My first Pre-filter is executed...");
+		
+		String requestPath = exchange.getRequest().getPath().toString();
+		logger.info("Request path = " + requestPath);
+
+		HttpHeaders headers = exchange.getRequest().getHeaders();
+		
+		Set<String> headerNames = headers.keySet();
+		
+		headerNames.forEach((headerName)-> {
+			
+			String headerValue = headers.getFirst(headerName);
+			logger.info(headerName + " " + headerValue);
+			
+		});
+		
+		return chain.filter(exchange);
 	}
 
 }
